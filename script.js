7f11f56eef8915d9646395fc10d2201ace1ce239
Raldy Achmad <87338157+raldyachmad@users.noreply.github.com>
2023-07-31 09:47:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-const beasiswaItem = document.querySelectorAll('div.beasiswa-item')
-const beasiswaForm = document.getElementById('beasiswa')
+const beasiswaItem = document.querySelectorAll("div.beasiswa-item");
+const beasiswaForm = document.getElementById("beasiswa");
 
-beasiswaItem.forEach(item => {
+beasiswaItem.forEach((item) => {
   item.addEventListener("click", () => {
-    beasiswaItem.forEach(item => item.classList.remove("active"));
+    beasiswaItem.forEach((item) => item.classList.remove("active"));
     item.classList.add("active");
-    beasiswaForm.value = item.querySelector('h2').innerText
+    beasiswaForm.value = item.querySelector("h2").innerText;
   });
-})
-const slide1=document.querySelector('.slide-1')
-const slide2=document.querySelector('.slide-2')
-const slide3=document.querySelector('.slide-3')
+});
+const slide1 = document.querySelector(".slide-1");
+const slide2 = document.querySelector(".slide-2");
+const slide3 = document.querySelector(".slide-3");
 function gotoSlide(slideNumber) {
   const slides = document.querySelectorAll(".slide");
   slides.forEach((slide) => (slide.style.display = "none"));
-  if(slideNumber==2){
-    if(beasiswaItem[0].classList.contains("active")|| beasiswaItem[1].classList.contains("active")){
-      slide1.style.display='none'
-      slide2.style.display='block'
-    } else  {
-      alert('Anda Belum Memilih Satupun, Pilih Salah Satu!')
+  if (slideNumber == 2) {
+    if (beasiswaItem[0].classList.contains("active") || beasiswaItem[1].classList.contains("active")) {
+      slide1.style.display = "none";
+      slide2.style.display = "block";
+    } else {
+      alert("Anda Belum Memilih Satupun, Pilih Salah Satu!");
     }
-  } else if (slideNumber==3){
-    checkFormSlide2()
-  } else if (slideNumber==1){
-    if (beasiswaItem[0].classList)
-    slide2.style.display='none';
-    slide3.style.display='none';
-    slide1.style.display='block';
+  } else if (slideNumber == 3) {
+    checkFormSlide2();
+  } else if (slideNumber == 1) {
+    if (beasiswaItem[0].classList) slide2.style.display = "none";
+    slide3.style.display = "none";
+    slide1.style.display = "block";
   }
 }
 
@@ -36,48 +35,48 @@ const btnSlide2 = document.getElementById("btnSlide2");
 
 function checkFormSlide2() {
   if (formSlide2.checkValidity()) {
-    alert('berhasil')
+    alert("berhasil");
     btnSlide2.disabled = true;
-    slide2.style.display='none';
-    slide3.style.display='block';
-    showResult()
+    slide2.style.display = "none";
+    slide3.style.display = "block";
+    showResult();
   } else {
     btnSlide2.disabled = false;
-    alert('Form Belum Lengkap!')
-    const input = document.querySelectorAll('input,select')
-    input.forEach((e)=>{
-      if (e.checkValidity()==false){
-        e.style.borderColor='red'
+    alert("Form Belum Lengkap!");
+    const input = document.querySelectorAll("input,select");
+    input.forEach((e) => {
+      if (e.checkValidity() == false) {
+        e.style.borderColor = "red";
       }
-      e.addEventListener('blur',()=>{
-        e.style.borderColor='';
-      })
-    })
+      e.addEventListener("blur", () => {
+        e.style.borderColor = "";
+      });
+    });
   }
 }
 
-const diterima = document.getElementById('diterima')
-const tidakDiterima = document.getElementById('tidakDiterima')
+const diterima = document.getElementById("diterima");
+const tidakDiterima = document.getElementById("tidakDiterima");
 // const abc = ;
 
 function showResult() {
   const ipkTerakhir = parseFloat(document.getElementById("ipk_terakhir").value);
+  const pisahNama = document.getElementById("nama").value.split(" ");
+  const fistName = pisahNama[0];
+  const inputBeasiswa = document.getElementById("beasiswa").value;
+
+  const diterima = document.getElementById("diterima");
+  const tidakDiterima = document.getElementById("tidakDiterima");
+
   if (ipkTerakhir >= 3) {
-    const pisahNama = document.getElementById('nama').value.split(" ")
-    const fistName = pisahNama[0];
-    const inputBeasiswa = document.getElementById('beasiswa').value;
-    // const kategoriBeasiswa = document.getElementById('namaBeasiswa');
-    tidakDiterima.style.display="none !important"
-    diterima.style.display='block';
-    diterima.querySelector('#nickname').textContent=fistName;
-    diterima.querySelector('#namaBeasiswa').textContent=inputBeasiswa;
-  }  else{
-    tidakDiterima.style.display='block';
-    const pisahNama = document.getElementById('nama').value.split(" ")
-    const fistName = pisahNama[0];
-    const inputBeasiswa = document.getElementById('beasiswa').value;
-    diterima.querySelector('#nickname').textContent=fistName;
-    diterima.querySelector('#namaBeasiswa').textContent=inputBeasiswa;
+    diterima.style.display = "block";
+    tidakDiterima.style.display = "none";
+    diterima.querySelector("#nickname").textContent = fistName;
+    diterima.querySelector("#namaBeasiswa").textContent = inputBeasiswa;
+  } else {
+    tidakDiterima.style.display = "block";
+    diterima.style.display = "none";
+    tidakDiterima.querySelector("#nickname").textContent = fistName;
+    tidakDiterima.querySelector("#namaBeasiswa").textContent = inputBeasiswa;
   }
-  
 }
