906dcba7b783c8b7fc2411e3d58bf4231f80a232
Raldy Achmad <87338157+raldyachmad@users.noreply.github.com>
2023-07-31 23:25:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ beasiswaItem.forEach((item) => {
     beasiswaForm.value = item.querySelector("h2").innerText;
   });
 });
+
 const slide1 = document.querySelector(".slide-1");
 const slide2 = document.querySelector(".slide-2");
 const slide3 = document.querySelector(".slide-3");
+
 function gotoSlide(slideNumber) {
   const slides = document.querySelectorAll(".slide");
   slides.forEach((slide) => (slide.style.display = "none"));
@@ -56,26 +58,78 @@ function checkFormSlide2() {
 
 const diterima = document.getElementById("diterima");
 const tidakDiterima = document.getElementById("tidakDiterima");
-// const abc = ;
+let dataMahasiswaArr = JSON.parse(sessionStorage.getItem('dataMahasiswaArr')) || [];
 
 function showResult() {
   const ipkTerakhir = parseFloat(document.getElementById("ipk_terakhir").value);
-  const pisahNama = document.getElementById("nama").value.split(" ");
-  const fistName = pisahNama[0];
+  const nama = document.getElementById("nama").value;
+  const email = document.getElementById("email").value;
+  const noHp = document.getElementById("no_hp").value;
+  const semester = document.getElementById("semester").value;
   const inputBeasiswa = document.getElementById("beasiswa").value;
+  const berkasSyarat = document.getElementById("berkas_syarat").value;
 
-  const diterima = document.getElementById("diterima");
-  const tidakDiterima = document.getElementById("tidakDiterima");
+  // Periksa apakah data berasal dari result.html
+  const fromResultPage = window.location.href.includes("result.html");
 
   if (ipkTerakhir >= 3) {
+    const dataMahasiswa = {
+      nama: nama,
+      email: email,
+      noHp: noHp,
+      semester: semester,
+      ipkTerakhir: ipkTerakhir,
+      beasiswa: inputBeasiswa,
+      berkasSyarat: berkasSyarat,
+    };
+
+    // Jika berasal dari result.html, gunakan data langsung dari session storage
+    if (fromResultPage) {
+      
+    } else {
+      dataMahasiswaArr.push(dataMahasiswa);
+      sessionStorage.setItem('dataMahasiswaArr', JSON.stringify(dataMahasiswaArr));
+    }
+
     diterima.style.display = "block";
     tidakDiterima.style.display = "none";
-    diterima.querySelector("#nickname").textContent = fistName;
+    diterima.querySelector("#nickname").textContent = nama;
     diterima.querySelector("#namaBeasiswa").textContent = inputBeasiswa;
   } else {
-    tidakDiterima.style.display = "block";
     diterima.style.display = "none";
-    tidakDiterima.querySelector("#nickname").textContent = fistName;
+    tidakDiterima.style.display = "block";
+    tidakDiterima.querySelector("#nickname").textContent = nama;
     tidakDiterima.querySelector("#namaBeasiswa").textContent = inputBeasiswa;
   }
 }
+
+
+
+// Load the data from session storage and display it in the table
+function loadResult() {
+  const dataMahasiswaArr = JSON.parse(sessionStorage.getItem('dataMahasiswaArr'));
+
+  if (dataMahasiswaArr) {
+    const tableBody = document.querySelector('#resultTable tbody');
+    tableBody.innerHTML = "";
+    console.log(tableBody);
+
+    dataMahasiswaArr.forEach((data, index) => {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${index + 1}</td>
+        <td>${data.nama}</td>
+        <td>${data.email}</td>
+        <td>${data.noHp}</td>
+        <td>${data.semester}</td>
+        <td>${data.ipkTerakhir}</td>
+        <td>${data.beasiswa}</td>
+        <td>${data.berkasSyarat}</td>
+      `;
+      tableBody.appendChild(row);
+    });
+  }
+}
+
+// Call the function to load the data when the page is ready
+document.addEventListener('DOMContentLoaded', loadResult);
